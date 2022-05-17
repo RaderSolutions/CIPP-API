d63e8ed44c23412db2b109d439cc5d4fb3f9acd0
Sarah Warner <swarner@radersolutions.com>
2022-05-17 13:29:41 -0500

    fix variables

--- LtListRatelVariables/run.ps1
+++ LtListRatelVariables/run.ps1
@@ -2,37 +2,38 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 Import-Module SimplySql
 Open-MySqlConnection -Server $ENV:LtServer -Database $ENV:LtDB -UserName $ENV:LtUser -Password $ENV:LtPass -Port 3306
 # get cwm id
+$requestJson = $Request | convertto-json
 $TenantFilter = $Request.Query.TenantFilter
 $cwaClientId = Get-LabtechClientId($TenantFilter)
-# Get Automate Auth Token
-$null = Connect-AzAccount -Identity
-# $token = Get-AzKeyVaultSecret -VaultName 'cipphglzr' -Name 'cwaRefreshToken' -AsPlainText
-try {
-    if ($Request.Query.Action -eq "Delete") { 
-        Invoke-SqlQuery -Query "DELETE from plugin_rader_ratel_did WHERE number=$($Request.Query.DIDNumber) AND client_id=$cwaClientId LIMIT 1;"
 
-    }
-    else { 
-        $didobj = $Request.body
-        Invoke-SqlQuery -Query "INSERT INTO plugin_rader_ratel_did (number, device_id, is_device_callerid, is_sync_scheduled, client_id, custom_dialplan) VALUES ($($didobj.Number), $($didobj.DeviceId), $($didobj.SetCallerId), 1, $cwaClientId, "") ON DUPLICATE KEY UPDATE device_id=$($didobj.DeviceId), is_device_callerid=$($didobj.SetCallerId), is_sync_scheduled=1, client_id=$cwaClientId, custom_dialplan="";"
-
-    }
-
-    $body = @{"Results" = "DID modifications stored in database" }
-
-} 
-catch { 
-    $body = @{"Results" = "Something went wrong." }
-    write-host $_.Exception
+if($Request.Query.family -and $Request.Query.key) { 
+    $family = $Request.Query.family
+    $key = $Request.Query.key
+    $table = Invoke-SqlQuery -Query "SELECT astFamily as 'Family', astKey as 'Key', astValue as 'Value', last_sync as 'LastRead' FROM plugin_rader_ratel_astdb WHERE client_id=$cwaClientId and astFamily =$family and astKey=$key" -AsDataTable
+} else { 
+    $table = Invoke-SqlQuery -Query "SELECT astFamily as 'Family', astKey as 'Key', astValue as 'Value', last_sync as 'LastRead' FROM plugin_rader_ratel_astdb WHERE client_id=$cwaClientId" -AsDataTable
 }
 
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+$variables = $table | Select-Object * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors 
+Close-SqlConnection
+$variableArray = @()
+$result
+
+if($variables.count -eq 1) { 
+    $variableArray += $variables
+    $result = $variableArray
+} else { 
+    $result = $variables | convertto-json
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
-        Body       = $body
+        Body       = $result
     })
