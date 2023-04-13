dff74e73d4fdf4b3c32fbda893b324e1a7ee6e89
Alex <63439547+smallfri321@users.noreply.github.com>
2023-04-13 15:24:31 -0500

    Added config blobs

---
+++ GrabConfigs/run.ps1
@@ -0,0 +1,17 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+
+# Import the Azure PowerShell module
+$APIName = $TriggerMetadata.FunctionName
+$null = Connect-AzAccount -Identity
+
+$storageAcc = Get-AzStorageAccount -ResourceGroupName $ENV:ResourceGroup -Name $ENV:StorageAcct
+$ctx = $storageAcc.Context
+$blobs = Get-AzStorageBlob -Container "configs" -Context $ctx -IncludeTag
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body       = $blobs
+})
