8c6fccda29d2955e2c5d4df028d730dd12aa6748
Alex <63439547+smallfri321@users.noreply.github.com>
2023-04-24 15:33:41 -0500

    Return Array of Blob json data

--- GrabConfigs/run.ps1
+++ GrabConfigs/run.ps1
@@ -2,16 +2,22 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-
 # Import the Azure PowerShell module
 $APIName = $TriggerMetadata.FunctionName
 $null = Connect-AzAccount -Identity
 
 $storageAcc = Get-AzStorageAccount -ResourceGroupName $ENV:ResourceGroup -Name $ENV:StorageAcct
 $ctx = $storageAcc.Context
-$blobs = Get-AzStorageBlob -Container "configs" -Context $ctx -IncludeTag
+$blobs = Get-AzStorageBlob -Container "configs" -Context $ctx -IncludeMetadata
+
+$jsonArray = @()
+foreach ($blob in $blobs) {
+    $blobName = $blob.Name
+    $blobContent = Get-AzStorageBlobContent -Container "configs" -Blob $blobName -Context $ctx -AsString
+    $jsonArray += $blobContent | ConvertFrom-Json
+}
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body       = $blobs
+    Body       = $jsonArray
 })
