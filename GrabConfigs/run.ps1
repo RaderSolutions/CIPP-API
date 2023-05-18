18da95b560c474fa5b95b4bce0d44d8e79a9eb8c
Alex <63439547+smallfri321@users.noreply.github.com>
2023-05-18 10:58:08 -0500

    created Blob array

--- GrabConfigs/run.ps1
+++ GrabConfigs/run.ps1
@@ -7,12 +7,26 @@ param($Request, $TriggerMetadata)
 $APIName = $TriggerMetadata.FunctionName
 $null = Connect-AzAccount -Identity
 
-$storageAcc = Get-AzStorageAccount -ResourceGroupName $ENV:ResourceGroup -Name $ENV:StorageAcct
-$ctx = $storageAcc.Context
-$blobs = Get-AzStorageBlob -Container "configs" -Context $ctx -IncludeTag
 
+
+# $storageAccountName = "your_storage_account_name"
+$containerName = "configs"
+
+$storageContext = Get-AzStorageAccount -ResourceGroupName $ENV:ResourceGroup -Name $ENV:StorageAcct
+$container = Get-AzStorageContainer -Name $containerName -Context $storageContext.Context
+
+$blobs = Get-AzStorageBlob -Container $containerName -Context $storageContext.Context
+$jsonContents = @()
+
+foreach ($blob in $blobs) {
+    if ($blob.Name -like "*.json") {
+        $blobContent = (Get-AzStorageBlobContent -Blob $blob.Name -Container $containerName -Context $storageContext.Context).Content
+        $decodedContent = [System.Text.Encoding]::UTF8.GetString($blobContent)
+        $jsonContents += $decodedContent
+    }
+}
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body       = $blobs
+    Body       = $jsonContents
 })
