5231da1a111c0415a8070054c4ea9b2f0c760145
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-08-16 13:05:02 -0500

    Update run.ps1

--- LtRatelPhoneBookEntry/run.ps1
+++ LtRatelPhoneBookEntry/run.ps1
@@ -22,24 +22,27 @@ try {
         Invoke-SqlQuery -Query "UPDATE plugin_rader_ratel_external_contacts SET dial=$($entryObj.Dial), prefix=$($entryObj.Salutation), first_name=$($entryObj.FirstName), second_name=$($entryObj.MiddleName), last_name=$($entryObj.LastName), suffix=$($entryObj.Suffix), primary_email=$($entryObj.Email), organization=$($entryObj.Organization), job_title=$($entryObj.JobTitle), location=$($entryObj.Location), notes=$($entryObj.Notes) WHERE client_id=$cwaClientId AND id=$($entryObj.ID) LIMIT 1;"
     }
     else { 
-        $entryObj = $Request.body
-        Invoke-SqlQuery -Query "INSERT INTO plugin_rader_ratel_external_contacts (dial, prefix, first_name, second_name, last_name, suffix,  primary_email, organization, job_title, location, notes, client_id, contact_type, is_from_fop)
-    VALUES
-       ($($entryObj.Dial),
-       $($entryObj.Salutation),
-       $($entryObj.FirstName),
-       $($entryObj.MiddleName),
-       $($entryObj.LastName),
-       $($entryObj.Suffix),
-       $($entryObj.Email),
-       $($entryObj.Organization),
-       $($entryObj.JobTitle),
-       $($entryObj.Location),
-       $($entryObj.Notes),
-       $cwaClientId,
-        'sip',
-        0)
-   ;"
+$entryObj = $Request.body
+Invoke-SqlQuery -Query @"
+INSERT INTO plugin_rader_ratel_external_contacts (dial, prefix, first_name, second_name, last_name, suffix, primary_email, organization, job_title, location, notes, client_id, contact_type, is_from_fop)
+VALUES (
+   '$($entryObj.Dial)',
+   '$($entryObj.Salutation)',
+   '$($entryObj.FirstName)',
+   '$($entryObj.MiddleName)',
+   '$($entryObj.LastName)',
+   '$($entryObj.Suffix)',
+   '$($entryObj.Email)',
+   '$($entryObj.Organization)',
+   '$($entryObj.JobTitle)',
+   '$($entryObj.Location)',
+   '$($entryObj.Notes)',
+   $cwaClientId,
+   'sip',
+   0
+);
+"@
+
     }
     $body = @{"Results" = "Phonebook Entry modifications stored in database" }
 
