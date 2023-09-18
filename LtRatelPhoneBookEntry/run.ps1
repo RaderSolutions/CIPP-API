2504a94f83a486be97b1072eff89b38c2aed6bb1
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-09-18 06:44:14 -0500

    LtRatelPhoneBookEntry logging

--- LtRatelPhoneBookEntry/run.ps1
+++ LtRatelPhoneBookEntry/run.ps1
@@ -45,7 +45,7 @@ LIMIT 1;
 
     }
     else { 
-        write-host $cwaClientId
+        write-host "add entry client id: $cwaClientId"
 #         Invoke-SqlQuery -Query @"
 # INSERT INTO labtech.plugin_rader_ratel_external_contacts (dial, prefix, first_name, second_name, last_name, suffix, primary_email, organization, job_title, location, notes, client_id, account_id, subscribe_to, custom_dial_action, contact_type, is_from_fop)
 # VALUES (
