1c6201746ec743c1325d779dfcec102fd66ba208
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-09-22 11:21:14 -0500

    LtRatelDIDs

--- LtRatelDIDs/run.ps1
+++ LtRatelDIDs/run.ps1
@@ -8,7 +8,8 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 Import-Module SimplySql
 Open-MySqlConnection -Server $ENV:LtServer -Database $ENV:LtDB -UserName $ENV:LtUser -Password $ENV:LtPass -Port 3306
 # get cwm id
-$TenantFilter = $Request.Query.TenantFilter
+# $TenantFilter = $Request.Query.TenantFilter
+TenantFilter = $Request.body.TenantFilter
 $cwaClientId = Get-LabtechClientId($TenantFilter)
 # Get Automate Auth Token
 $null = Connect-AzAccount -Identity
