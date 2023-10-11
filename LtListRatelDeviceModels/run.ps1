661b27d90e6e3b15984eacec4c34c99d2e2c00b3
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-11 14:25:58 -0500

    LtListRatelDeviceModels

--- LtListRatelDeviceModels/run.ps1
+++ LtListRatelDeviceModels/run.ps1
@@ -7,12 +7,18 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 Import-Module SimplySql
 Open-MySqlConnection -Server $ENV:LtServer -Database $ENV:LtDB -UserName $ENV:LtUser -Password $ENV:LtPass -Port 3306
+$table
+if($Request.Query.isProductTable){
+    write-host 'is product table'
+    write-host $Request.Query | convertto-json
+    $table = Invoke-SqlQuery -Query "select * from plugin_rader_ratel_product ORDER BY ID;" -AsDataTable
+}else{
+    $table = Invoke-SqlQuery -Query "select id as 'modelId',CONCAT(manufacturer_name,' ', Model) as Name from plugin_rader_ratel_product ORDER BY ID;" -AsDataTable
+}
 
 
-$table = Invoke-SqlQuery -Query "select id as 'modelId',CONCAT(manufacturer_name,' ', Model) as Name from plugin_rader_ratel_product ORDER BY ID;" -AsDataTable
-$allData = Invoke-SqlQuery -Query "select * from plugin_rader_ratel_product ORDER BY ID;" -AsDataTable
 # Associate values to output bindings by calling 'Push-OutputBinding'.
-$response = $allData | Select-Object * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors | convertto-json
+$response = $table | Select-Object * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors | convertto-json
 Close-SqlConnection
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
