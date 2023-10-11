0cb6c871e32ddb4c13769c79b984a7ee70e4318c
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-11 08:31:51 -0500

    LtListRatelPagingGroups

--- LtListRatelPagingGroups/run.ps1
+++ LtListRatelPagingGroups/run.ps1
@@ -17,38 +17,38 @@ write-host $cwaClientId
 if($Request.Query.MailboxId){
     $extensionId=$Request.Query.ExtensionId
     write-host $extensionId
-    $table = Invoke-SqlQuery -Query "SELECT
-    plugin_rader_ratel_pagegroups.pagegroup_extension AS 'Extension',
-    plugin_rader_ratel_pagegroups.pagegroup_name AS 'PageGroupName',
-    plugin_rader_ratel_device.id AS 'DeviceId',
-    plugin_rader_ratel_device.extension_number AS 'DeviceExt',
-    COALESCE(CONCAT(contacts.FirstName,' ',contacts.LastName),plugin_rader_ratel_device.label) AS 'User',
-    locations.Name AS 'Location',
-    plugin_rader_ratel_pagegroups.hidden_from_phonebook as 'HideFromPB'
-    FROM 
-    plugin_rader_ratel_pagegroups
-    LEFT JOIN 
-    plugin_rader_ratel_pagegroup_membership
-    ON
-    plugin_rader_ratel_pagegroup_membership.pagegroup_id=plugin_rader_ratel_pagegroups.id
-    LEFT JOIN
-    plugin_rader_ratel_device
-    ON 
-    plugin_rader_ratel_pagegroup_membership.device_id=plugin_rader_ratel_device.id
-    LEFT JOIN
-    contacts
-    ON
-    contacts.ContactID=plugin_rader_ratel_device.contact_id
-    LEFT JOIN
-    locations
-    ON 
-    locations.LocationID=plugin_rader_ratel_device.location_id
-    WHERE 
-    plugin_rader_ratel_pagegroups.client_id=$cwaClientId and plugin_rader_ratel_pagegroups.pagegroup_extension=$extensionId
+    # $table = Invoke-SqlQuery -Query "SELECT
+    # plugin_rader_ratel_pagegroups.pagegroup_extension AS 'Extension',
+    # plugin_rader_ratel_pagegroups.pagegroup_name AS 'PageGroupName',
+    # plugin_rader_ratel_device.id AS 'DeviceId',
+    # plugin_rader_ratel_device.extension_number AS 'DeviceExt',
+    # COALESCE(CONCAT(contacts.FirstName,' ',contacts.LastName),plugin_rader_ratel_device.label) AS 'User',
+    # locations.Name AS 'Location',
+    # plugin_rader_ratel_pagegroups.hidden_from_phonebook as 'HideFromPB'
+    # FROM 
+    # plugin_rader_ratel_pagegroups
+    # LEFT JOIN 
+    # plugin_rader_ratel_pagegroup_membership
+    # ON
+    # plugin_rader_ratel_pagegroup_membership.pagegroup_id=plugin_rader_ratel_pagegroups.id
+    # LEFT JOIN
+    # plugin_rader_ratel_device
+    # ON 
+    # plugin_rader_ratel_pagegroup_membership.device_id=plugin_rader_ratel_device.id
+    # LEFT JOIN
+    # contacts
+    # ON
+    # contacts.ContactID=plugin_rader_ratel_device.contact_id
+    # LEFT JOIN
+    # locations
+    # ON 
+    # locations.LocationID=plugin_rader_ratel_device.location_id
+    # WHERE 
+    # plugin_rader_ratel_pagegroups.client_id=$cwaClientId and plugin_rader_ratel_pagegroups.pagegroup_extension=$extensionId
     
-    ORDER BY 
-    plugin_rader_ratel_pagegroups.pagegroup_name
-    " -AsDataTable 
+    # ORDER BY 
+    # plugin_rader_ratel_pagegroups.pagegroup_name
+    # " -AsDataTable 
 } else {
 $table = Invoke-SqlQuery -Query "SELECT
 plugin_rader_ratel_pagegroups.pagegroup_extension AS 'Extension',
