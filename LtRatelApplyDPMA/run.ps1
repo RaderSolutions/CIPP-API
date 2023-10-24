4c1a2753be5549b012cc8ab30c884926e2efa631
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-24 09:45:28 -0500

    LtRatelApplyDPMA

--- LtRatelApplyDPMA/run.ps1
+++ LtRatelApplyDPMA/run.ps1
@@ -21,11 +21,11 @@ write-host $cwaClientId
 write-host 'entryObj/license'
 write-host $entryObj.LicenseKey
 try {
-#     Invoke - SqlQuery - Query @"
-#     INSERT INTO plugin_rader_ratel_configuration (client_id,parameter,value) 
-#     VALUES ('$cwaClientId','dpma_license_key','$($entryObj.LicenseKey)') 
-#     ON DUPLICATE KEY UPDATE VALUE='$($entryObj.LicenseKey)'; 
-# "@    
+    Invoke - SqlQuery - Query @"
+    INSERT INTO plugin_rader_ratel_configuration (client_id,parameter,value) 
+    VALUES ('$cwaClientId','dpma_license_key','$($entryObj.LicenseKey)') 
+    ON DUPLICATE KEY UPDATE VALUE='$($entryObj.LicenseKey)'; 
+"@    
     $body = @{"Results" = "DPMA License Applied" }
 
 } 
