b9aac48a26d3d48f603a9a48f0e20b8aa9b99c21
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-25 07:49:54 -0500

    LtRatelHidePagingGroupDevice

--- LtRatelHidePagingGroupDevice/run.ps1
+++ LtRatelHidePagingGroupDevice/run.ps1
@@ -19,41 +19,9 @@ $null = Connect-AzAccount -Identity
 $entryObj = $Request.body
 write-host $cwaClientId
 try {
-Invoke-SqlQuery -Query @"
-SELECT
-plugin_rader_ratel_pagegroups.pagegroup_extension AS 'Extension',
-plugin_rader_ratel_pagegroups.pagegroup_name AS 'PageGroup Name',
-plugin_rader_ratel_device.id AS 'Device ID',
-plugin_rader_ratel_device.extension_number AS 'Device Ext',
-COALESCE(CONCAT(contacts.FirstName,' ',contacts.LastName),plugin_rader_ratel_device.label) AS 'User',
-locations.Name AS 'Location',
-plugin_rader_ratel_pagegroups.hidden_from_phonebook as 'Hide from PB'
-FROM 
-plugin_rader_ratel_pagegroups
-LEFT JOIN 
-plugin_rader_ratel_pagegroup_membership
-ON
-plugin_rader_ratel_pagegroup_membership.pagegroup_id=plugin_rader_ratel_pagegroups.id
-LEFT JOIN
-plugin_rader_ratel_device
-ON 
-plugin_rader_ratel_pagegroup_membership.device_id=plugin_rader_ratel_device.id
-LEFT JOIN
-contacts
-ON
-contacts.ContactID=plugin_rader_ratel_device.contact_id
-LEFT JOIN
-locations
-ON 
-locations.LocationID=plugin_rader_ratel_device.location_id
-WHERE 
-plugin_rader_ratel_pagegroups.client_id='$cwaClientId'
-
-ORDER BY 
-plugin_rader_ratel_pagegroups.pagegroup_name
-
-"@
-    $body = @{"Results" = "Phonebook Entry modifications stored in database" }
+    write-host 'request body'
+    write-host $Request.body | convertto-json
+    # $body = @{"Results" = "Phonebook Entry modifications stored in database" }
 
 } 
 catch { 
