28f7ce9862aa495e440b4783015dc9f3c7fc600b
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2024-03-20 11:35:51 -0500

    ltlistconfigs name change

---
+++ LtListConfigs/run.ps1
@@ -0,0 +1,36 @@
+
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+# Import the Azure PowerShell module
+$APIName = $TriggerMetadata.FunctionName
+$null = Connect-AzAccount -Identity
+
+$containerName = "configs"
+
+$storageContext = Get-AzStorageAccount -ResourceGroupName $ENV:ResourceGroup -Name $ENV:StorageAcct
+$container = Get-AzStorageContainer -Name $containerName -Context $storageContext.Context
+
+$blobs = Get-AzStorageBlob -Container $containerName -Context $storageContext.Context
+$jsonContents = @()
+
+foreach ($blob in $blobs) {
+    if ($blob.Name -like "*.json") {
+        $blobContent = (Get-AzStorageBlobContent -Blob $blob.Name -Container $containerName -Context $storageContext.Context).Content
+        $decodedContent = [System.Text.Encoding]::UTF8.GetString($blobContent)
+        $jsonContents += $decodedContent
+    }
+}
+
+$responseData = @{
+    "jsonContents" = $jsonContents
+    "blobs" = $blobs
+}
+
+$responseJson = $responseData | ConvertTo-Json
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body       = $responseJson
+})
